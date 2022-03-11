b2d22082901c7babbb93628fe7149d5f64830668
Arushay <piecemaster780@gmail.com>
2022-03-11 16:03:06 +0100

    Api and json files created

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -10,7 +10,7 @@ const slug = () => {
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-col text-center w-full mb-20">
                         <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 py-2">Title {slug}</h1>
-                        <hr />
+                       
                         <p className="leading-relaxed text-base py-2">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
                     </div>
                 </div>
