a901191260560fb784cc1a6b642c90d0911a242f
Arushay <piecemaster780@gmail.com>
2022-03-11 14:10:33 +0100

    worked on Slug n blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const blog = () => {
   return (
@@ -6,15 +7,16 @@ const blog = () => {
          <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-col text-center w-full mb-20">
-              <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
+              
               <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
+              <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
             </div>
             <div className="flex flex-wrap -m-4">
               <div className="p-4 md:w-1/3">
                 <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                   <div className="flex items-center mb-3">
-
-                    <h2 className="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
+                  <Link href={'/blogpost/shooting stars'}>
+                    <h2 className="text-gray-900 text-lg title-font font-medium cursor-pointer">Shooting Stars</h2></Link>
                   </div>
                   <div className="flex-grow">
                     <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
---
+++ pages/blogpost/[slug].js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const slug = () => {
+    const router = useRouter();
+    const { slug } = router.query;
+    return (
+        <div>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 py-2">Title {slug}</h1>
+                        <hr />
+                        <p className="leading-relaxed text-base py-2">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default slug;
+
+
+// i update this page on 11 march and it worked 
