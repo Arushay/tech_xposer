655c4c136ea77a02850ff9fef476c18bb3867f55
Arushay <piecemaster780@gmail.com>
2022-03-14 13:35:23 +0100

    Api created & used

--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
 
 const blog = () => {
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+  fetch('http://localhost:3000/api/blogs').then((a)=>{
+    return a.json()})
+    .then((parsed)=>{
+      setBlogs(parsed)
+    })
+  }, [])
+  
+
+
   return (
     <div>
-         <section className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-col text-center w-full mb-20">
-              
-              <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
-              <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
-            </div>
-            <div className="flex flex-wrap -m-4">
+     
+        <div className="flex flex-col text-center w-full mb-20"> 
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
+      </div>
+      {blogs.map((blogItem)=>{
+        return <section className="text-gray-600 body-font">
+          <div className="flex flex-col text-center w-full mb-20"> 
+           <div className="container px-5 py-24 ">
+            <div className="flex flex-wrap -m-4" key={blogItem.slug}>
               <div className="p-4 md:w-1/3">
                 <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                   <div className="flex items-center mb-3">
-                  <Link href={'/blogpost/shooting stars'}>
-                    <h2 className="text-gray-900 text-lg title-font font-medium cursor-pointer">Shooting Stars</h2></Link>
+                  <Link href={`/blogpost/${blogItem.slug}`}>
+                    <h2 className="text-gray-900 text-lg title-font font-medium cursor-pointer">{blogItem.title}</h2></Link>
                   </div>
                   <div className="flex-grow">
-                    <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                    <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                    <p className="leading-relaxed text-base">{blogItem.content.substr(0, 270)}...</p>
+                    <a className="mt-3 text-indigo-500 inline-flex items-center cursor-pointer">Learn More
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                       </svg>
@@ -28,42 +41,13 @@ const blog = () => {
                   </div>
                 </div>
               </div>
-              <div className="p-4 md:w-1/3">
-                <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-                  <div className="flex items-center mb-3">
-
-                    <h2 className="text-gray-900 text-lg title-font font-medium">The Catalyzer</h2>
-                  </div>
-                  <div className="flex-grow">
-                    <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                    <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="p-4 md:w-1/3">
-                <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-                  <div className="flex items-center mb-3">
-
-                    <h2 className="text-gray-900 text-lg title-font font-medium">Neptune</h2>
-                  </div>
-                  <div className="flex-grow">
-                    <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                    <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
         </section>
 
+      })}
+         
     </div>
   )
 }
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const slug = () => {
+    const [blog, setBlog] = useState()
     const router = useRouter();
-    const { slug } = router.query;
+    useEffect(() => {
+        if (!router.isReady) return;
+        const { slug } = router.query;
+        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+            return a.json()
+        })
+            .then((parsed) => {
+                setBlog(parsed)
+            })
+    }, [router.isReady])
+
+   
     return (
         <div>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col text-center w-full mb-20">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 py-2">Title {slug}</h1>
-                       
-                        <p className="leading-relaxed text-base py-2">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+                    <div className="flex flex-col text-center w-100 mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 py-2">{blog && blog.title}</h1>
+                        <p className="leading-relaxed text-base py-2">{blog && blog.content}</p>
                     </div>
                 </div>
             </section>
